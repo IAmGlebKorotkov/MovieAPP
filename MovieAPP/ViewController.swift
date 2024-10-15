93ffa71bef7e4b93e50d1cfab6e8634c4ce743f9
glebkorotkov <ukmmm200@yandex.ru>
2024-10-16 01:25:07 +0700

    Initial Commit

---
+++ MovieAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPP
+//
+//  Created by Gleb Korotkov on 16.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
